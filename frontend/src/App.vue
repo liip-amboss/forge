e3956d639afc132c088721ac69b20eb63d8bd186
Raphi <raphael.antonietti@liip.ch>
2019-10-17 08:53:19 +0200

    Remove Transitions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,11 +9,7 @@
       </div>
     </header>
 
-    <button class="bg-gray-600 hover:bg-gray-500 transition-1000">
-      Hover me for a lighter background
-    </button>
-
-    <div class="cssgrid p8 grid-columns-2 grid-gap-8">
+    <div class="css-grid grid-columns-2">
       <p>one</p>
       <p>two</p>
       <p>three</p>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -196,54 +196,6 @@ module.exports = {
       outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
       none: 'none',
     },
-    transitionProperty: {
-      // defaults to these values
-      none: 'none',
-      all: 'all',
-      color: 'color',
-      bg: 'background-color',
-      border: 'border-color',
-      colors: ['color', 'background-color', 'border-color'],
-      opacity: 'opacity',
-      transform: 'transform',
-    },
-    transitionDuration: {
-      // defaults to these values
-      default: '250ms',
-      '0': '0ms',
-      '100': '100ms',
-      '250': '250ms',
-      '500': '500ms',
-      '750': '750ms',
-      '1000': '1000ms',
-    },
-    transitionTimingFunction: {
-      // defaults to these values
-      default: 'ease',
-      linear: 'linear',
-      ease: 'ease',
-      'ease-in': 'ease-in',
-      'ease-out': 'ease-out',
-      'ease-in-out': 'ease-in-out',
-    },
-    transitionDelay: {
-      // defaults to these values
-      default: '0ms',
-      '0': '0ms',
-      '100': '100ms',
-      '250': '250ms',
-      '500': '500ms',
-      '750': '750ms',
-      '1000': '1000ms',
-    },
-    willChange: {
-      // defaults to these values
-      auto: 'auto',
-      scroll: 'scroll-position',
-      contents: 'contents',
-      opacity: 'opacity',
-      transform: 'transform',
-    },
     container: {},
     cursor: {
       auto: 'auto',
@@ -548,6 +500,5 @@ module.exports = {
       },
       variants: ['responsive'],
     }),
-    require('tailwindcss-transitions')(),
   ],
 }
