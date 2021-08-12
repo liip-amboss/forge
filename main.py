b4cf1b9e54e9dedbec0ddac9be675e27ee79444f
John Gannon <john.gannon@liip.ch>
2021-08-12 17:21:43 +0200

    Create gitlab project, git repo and add secrets to project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import fire
 import logging
 import doctl
+import gitlab
+from git import Repo
+from cookiecutter.main import cookiecutter
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
@@ -14,7 +17,7 @@ class Builder:
     CLI used to build new forge applications
     """
 
-    def create_project_server(self, project_name: str, personal_ssh_key_fingerprint: str):
+    def create_project(self, project_name: str, personal_ssh_key_fingerprint: str):
         """
         Create DO droplet running docker
         :param project_name: Name of project
@@ -35,6 +38,9 @@ class Builder:
         droplet_id = droplet[0]['id']
         server_ip = self.get_droplet_ip(droplet_id)
         self.create_domain(project_name=project_name, ip_address=server_ip)
+        project_dir = self.create_project_folder()
+        git_ssh_url = self.create_gitlab_repo(project_name=project_name, deploy_ssh_key=deploy_key['private'])
+        self.push_project(git_url=git_ssh_url, project_dir=project_dir)
 
     def create_domain(self, project_name: str, ip_address):
         """
@@ -90,12 +96,37 @@ class Builder:
 
         return droplet[0]['networks']['v4'][1]['ip_address']
 
-    def create_project_repo(self, project_name: str):
+    def create_project_folder(self):
         """
-        Generates everything required for a new forge project including DO droplets and Gitlab projects
-        :param project_name: Name of project
+        Generates project with given name from cookiecutter template
         """
-        logger.info(f"Project name {project_name} ...")
+        project_dir = cookiecutter('.', output_dir='../')
+        return project_dir
+
+    def create_gitlab_repo(self, project_name, deploy_ssh_key):
+        """
+        Generates project with given name from cookiecutter template
+        """
+        gl = gitlab.Gitlab.from_config('liip', ['/etc/python-gitlab.cfg'])
+        group_id = gl.groups.list(search='amboss')[0].id
+        project = gl.projects.create({'name': project_name, 'namespace_id': group_id})
+        project.variables.create({'key': 'STAGING_DOMAIN', 'value': f'{project_name}-dev.bedev.liip.ch'})
+        project.variables.create({'key': 'STAGING_SSH_PRIVATE_KEY', 'value': deploy_ssh_key})
+        project.deploytokens.create({
+            'name': 'gitlab-deploy-token',
+            'scopes': ['read_registry', 'read_repository'],
+            'username': '',
+            'expires_at': ''
+        })
+
+        return project.ssh_url_to_repo
+
+    def push_project(self, git_url, project_dir):
+        repo = Repo.init(project_dir)
+        repo.git.add(all=True)
+        repo.index.commit("initial commit")
+        remote = repo.create_remote('origin', url=git_url)
+        remote.push(refspec='{}:{}'.format('master', 'origin/master'))
 
 
 if __name__ == "__main__":
