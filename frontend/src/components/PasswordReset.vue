fc078bc1436abe730d6674704bcf7ecd96da3429
Medix13 <mahdi.pk98@gmail.com>
2022-08-24 14:59:08 +0200

    debug the passwordReset commponent

--- frontend/src/components/PasswordReset.vue
+++ frontend/src/components/PasswordReset.vue
@@ -11,33 +11,33 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue';
 import PasswordResetEmailForm from '@/components/PasswordResetEmailForm.vue';
 import PasswordResetPasswordForm from '@/components/PasswordResetPasswordForm.vue';
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 
-const router = useRouter()
+const router = useRouter();
 
-const email = ref('')
-const isPasswordFormShown = ref(false)
+const email = ref('');
+const isPasswordFormShown = ref(false);
 
 const token = computed(() => {
-          const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get('token');
-})
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('token');
+});
 
 const redirectToForgotPassword = () => {
-  router.push({ name: 'forgot-password' })
-}
+  router.push({ name: 'forgot-password' });
+};
 
 const showPasswordForm = (email) => {
-  email.value = email
-  isPasswordFormShown.value = true
-}
+  email.value = email;
+  isPasswordFormShown.value = true;
+};
 
 onMounted(() => {
-  if(!token.value) {
-    redirectToForgotPassword()
+  if (!token.value) {
+    redirectToForgotPassword();
   }
-})
+});
 </script>
