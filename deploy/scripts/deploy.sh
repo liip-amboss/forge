69b9771aeae5379369538dd7156e142b0178c5e1
John Gannon <john.gannon@liip.ch>
2022-10-18 16:22:22 +0200

    Simplify image name definitions and include commit hash in all images to allow all deployments to be repeted. Upgrade forge helm version

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -4,15 +4,15 @@ reg_user=$1
 reg_password=$2
 reg=$3
 project_path=$4
-docker_server_tag=$5
+docker_image_name=$5
 cd $HOME/srv/app/
 
 echo "Pulling the docker images"
 echo ${reg_password} | docker login -u ${reg_user} --password-stdin ${reg}
-docker pull ${reg}/${project_path}:backend_${docker_server_tag}
-docker pull ${reg}/${project_path}:frontend_${docker_server_tag}
-docker pull ${reg}/${project_path}:docs_${docker_server_tag}
-docker pull ${reg}/${project_path}:styleguide_${docker_server_tag}
+docker pull ${reg}/${project_path}/backend:${docker_image_name}
+docker pull ${reg}/${project_path}/frontend:${docker_image_name}
+docker pull ${reg}/${project_path}/docs:${docker_image_name}
+docker pull ${reg}/${project_path}/styleguide:${docker_image_name}
 
 echo "Starting the docker containers"
 docker-compose up -d
